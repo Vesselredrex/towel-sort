6f02fb24b658c830d62e67119d7ffff093aab32c
Dymitr Czykanenko <dymitrczykanenko@mac.home>
2025-09-06 17:43:34 +0200

    refactor: improve towel sort performance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
-};
+function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) {
+      // Парні рядки (0, 2, 4, ...) - додаємо в прямому порядку
+      for (let j = 0; j < matrix[i].length; j += 1) {
+        result.push(matrix[i][j]);
+      }
+    } else {
+      // Непарні рядки (1, 3, 5, ...) - додаємо в зворотньому порядку
+      for (let j = matrix[i].length - 1; j >= 0; j -= 1) {
+        result.push(matrix[i][j]);
+      }
+    }
+  }
+
+  return result;
+}
+
+module.exports = towelSort;
