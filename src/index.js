3ba4ff647a7a622d5348432b9945a2dde6ba2dd9
Dymitr Czykanenko <dymitrczykanenko@mac.home>
2025-09-06 17:45:18 +0200

    Incorect lines has been removed

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ function towelSort(matrix) {
 
   for (let i = 0; i < matrix.length; i += 1) {
     if (i % 2 === 0) {
-      // Парні рядки (0, 2, 4, ...) - додаємо в прямому порядку
       for (let j = 0; j < matrix[i].length; j += 1) {
         result.push(matrix[i][j]);
       }
     } else {
-      // Непарні рядки (1, 3, 5, ...) - додаємо в зворотньому порядку
       for (let j = matrix[i].length - 1; j >= 0; j -= 1) {
         result.push(matrix[i][j]);
       }
